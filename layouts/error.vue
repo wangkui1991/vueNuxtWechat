70605ae96e44585bc7d8ae29ee8ad9576c1582c7
smallDada <872505550@qq.com>
2018-08-28 19:46:17 +0800

    补漏

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,13 +1,16 @@
 <template>
   <section class="container">
-    <img src="../static/img/logo.png" alt="Nuxt.js Logo" />
+    <img src="../static/img/logo.png"
+         alt="Nuxt.js Logo" />
     <h1 class="title">
       {{ error.statusCode }}
     </h1>
     <h2 class="info">
       {{ error.message }}
     </h2>
-    <nuxt-link class="button" to="/" v-if="error.statusCode === 404">
+    <nuxt-link class="button"
+               to="/"
+               v-if="error.statusCode === 404">
       Homepage
     </nuxt-link>
   </section>
@@ -19,19 +22,16 @@ export default {
 </script>
 
 <style scoped>
-.title
-{
+.title {
   margin-top: 15px;
   font-size: 5em;
 }
-.info
-{
+.info {
   font-weight: 300;
   color: #9aabb1;
   margin: 0;
 }
-.button
-{
+.button {
   margin-top: 50px;
 }
 </style>
