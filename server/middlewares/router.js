13b43025ee3b4ff09a3c28283de7f41d00c9f491
wangkui1991 <872505550@qq.com>
2018-12-19 20:10:19 +0800

    素材管理

--- server/middlewares/router.js
+++ server/middlewares/router.js
@@ -2,11 +2,20 @@ import Router from 'koa-router'
 import reply from '../wechat/reply'
 import config from '../config'
 import wechatMiddle from '../wechat-lib/middleware'
+import { resolve } from 'path'
 
 export const router = app => {
   const router = new Router()
 
   router.all('/wechat-hear', wechatMiddle(config.wechat, reply))
+
+  router.get('/upload', async (ctx, next) => {
+    let mp = require('../wechat')
+    let client = mp.getWechat()
+
+    const data = await client.handle('uploadMaterial', 'image', resolve(__dirname, '../../ice.jpg'), { type: 'image' })
+    console.log('5', data)
+  })
 
   app.use(router.routes())
   app.use(router.allowedMethods())
--- server/wechat-lib/index.js
+++ server/wechat-lib/index.js
@@ -1,9 +1,35 @@
 import request from 'request-promise'
+import formstream from 'formstream'
+import fs from 'fs'
+import path from 'path'
+import * as _ from 'lodash'
 
 const base = 'https://api.weixin.qq.com/cgi-bin/'
 const api = {
-  accessToken: base + 'token?grant_type=client_credential'
+  accessToken: base + 'token?grant_type=client_credential',
+  temporary: {
+    upload: base + 'media/upload?',
+    fetch: base + 'media/get?'
+  },
+  permanent: {
+    upload: base + 'material/add_material?',
+    uploadNews: base + 'material/add_news?',
+    uploadNewsPic: base + 'material/uploadimg?',
+    fetch: base + 'material/get_material?',
+    del: base + 'material/del_material?',
+    update: base + 'material/update_news',
+    count: base + 'material/get_materialcount?',
+    batch: base + 'material/batchget_material?'
+  }
+}
 
+function statFile (filepath) {
+  return new Promise((resolve, reject) => {
+    fs.stat(filepath, (err, stat) => {
+      if (err) reject(err)
+      else resolve(stat)
+    })
+  })
 }
 
 export default class Wechat {
@@ -18,11 +44,11 @@ export default class Wechat {
   }
 
   async request (options) {
-    options = Object.assign({}, options, {
-      json: true
-    })
+    options = Object.assign({}, options, { json: true })
     try {
+      console.log(132)
       const res = await request(options)
+
       return res
     } catch (error) {
       console.error(error)
@@ -38,11 +64,12 @@ export default class Wechat {
     return data
   }
   async updateAccessToken () {
-    const url = api.accessToken + '&appid=' + this.appID + '&secret=' + this.appSecret
+    const url =
+      api.accessToken + '&appid=' + this.appID + '&secret=' + this.appSecret
     const data = await this.request({
       url: url
     })
-    const now = (new Date().getTime())
+    const now = new Date().getTime()
     const expiresIn = now + (data.expires_in - 20) * 1000
     data.expires_in = expiresIn
     return data
@@ -53,11 +80,63 @@ export default class Wechat {
       return false
     }
     const expiresIn = data.expires_in
-    const now = (new Date().getTime())
+    const now = new Date().getTime()
     if (now < expiresIn) {
       return true
     } else {
       return false
     }
+  }
+
+  async handle (operation, ...args) {
+    const tokenData = await this.fetchAccessToken()
+    const options = await this[operation](tokenData.access_token, ...args)
+    console.log('3', options)
+    const data = await this.request(options)
+    console.log('data', data)
+    return data
+  }
+
+  async uploadMaterial (token, type, material, permanent) {
+    let form = {}
+    let url = api.temporary.upload
+
+    if (permanent) {
+      url = api.permanent.upload
+
+      _.extend(form, permanent)
+    }
+
+    if (type === 'pic') {
+      url = api.permanent.uploadNewsPic
+    }
+
+    if (type === 'news') {
+      url = api.permanent.uploadNews
+      form = material
+    } else {
+      form.media = fs.createReadStream(material)
+    }
+    let uploadUrl = url + 'access_token=' + token
+
+    if (!permanent) {
+      uploadUrl += '&type=' + type
+    } else {
+      form.access_token = token
+    }
+
+    const options = {
+      method: 'POST',
+      url: uploadUrl,
+      json: true
+    }
+
+    if (type === 'news') {
+      options.body = form
+    } else {
+      options.formData = form
+    }
+
+    return options
   }
 }
