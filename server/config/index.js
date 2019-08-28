a6ab73d7e14f38c007dd6ad22f3917fe83b7c7fb
kui wang <kuiwang@kuis-MacBook-Pro.local>
2019-08-28 15:03:39 +0800

    appsecret过期,重制

--- server/config/index.js
+++ server/config/index.js
@@ -1,8 +1,10 @@
 export default {
   db: 'mongodb://localhost/iceAndFile',
   wechat: {
-    appID: 'wx498f9eef0a492181',
-    appSecret: 'b1e7967cec29de26748ce5471097a85e',
-    token: 'kui'
+    appID: 'wxf8099bba1dc7c1a4',
+    // appSecret: 'b1e7967cec29de26748ce5471097a85e',
+    appSecret: '66680656f219fcc780999812d7911824',
+    token: 'kui',
+    EncodingAESKey: 'zSOWrgtazqcluuJj5uT6eXwVwIjp1T3KIkgbkhT5DnP'
   }
 }
