8a67f33d906a3d7e5798f14532b2eebae989ad95
kui wang <kuiwang@kuis-MacBook-Pro.local>
2019-10-30 17:23:15 +0800

    修改服务器

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   .container
-    img(src="../static/img/logo.png")
+    //- img(src="~static/images/logo.png")
     h1.title {{ error.statusCode }}
     h2.info {{ error.message }}
     nuxt-link.button(to='/' v-if='error.statusCode === 404') Homepage
--- server/config/index.js
+++ server/config/index.js
@@ -1,10 +1,10 @@
 export default {
-  db: 'mongodb://localhost/iceAndFile',
+  db: 'mongodb://localhost/wechat',
   wechat: {
-    // appID: 'wx498f9eef0a492181',
-    // appSecret: '66680656f219fcc780999812d7911824',
-    appID: 'wx87043d3cc5e18213',
-    appSecret: '3d6f510195b496512846f6875b60886b',
+    // appID: 'wx87043d3cc5e18213',
+    // appSecret: '3d6f510195b496512846f6875b60886b',
+    appID: 'wxf8099bba1dc7c1a4',
+    appSecret: '7aaeb0869cc60af9a7953daced7c95b5',
 
     token: 'kui',
     EncodingAESKey: 'zSOWrgtazqcluuJj5uT6eXwVwIjp1T3KIkgbkhT5DnP'
